14ca2bcb8e6c3e784a2b1b93f01cd3f97723f54a
BBBBlarry <blarry.personal@gmail.com>
2021-03-07 13:17:42 -0800

    Audio cutter is kind of working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Authenticate from "./pages/authenticate/Authenticate";
 import Post from "./pages/post/Post";
-import SnippingPage from "./pages/snippingPage/SnippingPage";
+import SnippingPage from "./pages/snippingPage/SnippingPage.jsx";
 import Home from "./pages/home/Home";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
