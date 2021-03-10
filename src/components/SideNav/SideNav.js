c2a32b1183620689ed30e7b1c8d56c3e57cf97e0
Dong Kyun Kim <david.kim@richmond.edu>
2021-03-10 12:07:00 -0500

    just changing color of font in side nav

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -6,7 +6,6 @@ import auxinLogo from "../../media/Auxin.png";
 import { Link } from "react-router-dom";
 
 const SideNav = ({ history, location }) => {
-
   const handleActive = pathname => {
     if (pathname === "/") {
       return "/Home";
@@ -44,23 +43,24 @@ const SideNav = ({ history, location }) => {
             style: ({ $active, $theme }) => {
               if (!$active)
                 return {
-                  fontSize: '18px',
-                  fontWeight:'500',
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  color: "rgba(255,255,255,0.60)",
+
                   ":hover": {
                     backgroundColor: $theme.colors.mono800,
-                    color: $theme.colors.white,
-                    boxShadow: $theme.lighting.shadow400,
-                    
+                    color: "rgba(255,255,255,0.87)",
+                    boxShadow: $theme.lighting.shadow400
                   }
                 };
               return {
-                fontSize: '18px',
-                fontWeight:'500',
+                color: "rgba(255,255,255,0.87)",
+                fontSize: "18px",
+                fontWeight: "500",
                 backgroundColor: $theme.colors.mono700,
                 borderLeftColor: "#85d6af",
                 backgroundImage: "",
-                boxShadow: $theme.lighting.shadow600,
-
+                boxShadow: $theme.lighting.shadow600
               };
             }
           }
